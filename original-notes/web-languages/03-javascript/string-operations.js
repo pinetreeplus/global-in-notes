c33f13471622775768922bf6556fa92e22fbd1d0
Jinwoo Jeong <pinetreeplus@proton.me>
2026-04-27 09:38:03 +0900

    README 출처 섹션 수정

--- original-notes/web-languages/03-javascript/string-operations.js
+++
@@ -1,7 +0,0 @@
-// indexOf() example
-const firstOccurrence = tagline.indexOf("developers");
-const secondOccurrence = tagline.indexOf("developers", firstOccurrence + 1);
-
-console.log(firstOccurrence); // 20
-console.log(secondOccurrence); // 35
-
